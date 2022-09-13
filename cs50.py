e0547ddc025d8ae31502feb4cfaf9fc64d233803
Zachary Boelter <zboelter@gmail.com>
2022-09-13 11:37:58 -0700

    squared and hello funcs

--- cs50.py
+++ cs50.py
@@ -1,6 +1,27 @@
-x = float(input('What is x? '))
-y = float(input('What is y? '))
+# x = float(input('What is x? '))
+# y = float(input('What is y? '))
 
-z = round(x / y)
+# z = round(x / y)
 
-print(f'{z:.2f}')
+# print(f'{z:.2f}')
+
+# def main():
+#     name = input("whats your name? ")
+#     hello(name)
+
+# def hello(to="World"):
+#     return(f'Hello, {to}')
+
+
+# print(main())
+
+
+
+def main():
+    x = int(input('What is x? '))
+    print(f'x squared is {squared(x)}')
+
+def squared(num):
+    return pow(num, 2)  
+
+main()
