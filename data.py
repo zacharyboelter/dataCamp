78aabf77856dea51b91186165d75e06ac8059b49
Zachary Boelter <zboelter@gmail.com>
2022-09-14 15:37:10 -0700

    fifa heights

--- data.py
+++ data.py
@@ -17,3 +17,30 @@ print("Standard Deviation: " + str(stddev))
 
 # Print out correlation between first and second column. Replace 'None'
 corr = np.corrcoef(np_baseball[:, 0], np_baseball[:, 1])    #compare the first and second columns to see the correlation READ THE INSTRUCTIONS DUMMY
+
+
+# heights and positions are available as lists
+
+# Import numpy
+import numpy as np
+
+# Convert positions and heights to numpy arrays: np_positions, np_heights
+np_postitions = np.array(positions)
+np_heights = np.array(heights)
+
+
+# Heights of the goalkeepers: gk_heights
+gk_heights = np_heights[np_postitions == 'GK']
+
+# Heights of the other players: other_heights
+other_heights = np_heights[np_postitions != 'GK']
+
+# Print out the median height of goalkeepers. Replace 'None'
+print("Median height of goalkeepers: " + str(np.median(gk_heights)))
+
+# Print out the median height of other players. Replace 'None'
+print("Median height of other players: " + str(np.median(other_heights)))
+
+#Assign fifa roles and heights to numpy arrays.
+#assign goalkeep hights and other heights
+#average of both to see if one is higher than other
