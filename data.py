e0e2cdc16d974809e706ea00cecd8c9b5b503600
Zachary Boelter <zboelter@gmail.com>
2022-09-14 15:28:53 -0700

    numpy for mean, median, standard deviation and correlation for stats

--- data.py
+++ data.py
@@ -1,6 +1,19 @@
+# np_baseball is available
+
+# Import numpy
 import numpy as np
 
-np.array([1, 2, 3, 4])
+# Print mean height (first column)
+avg = np.mean(np_baseball[:,0])
+print("Average: " + str(avg))
 
-np_height = np.array([])
+# Print median height. Replace 'None'
+med = np.median(np_baseball[:, 0])
+print("Median: " + str(med))
 
+# Print out the standard deviation on height. Replace 'None'
+stddev = np.std(np_baseball[:, 0])
+print("Standard Deviation: " + str(stddev))
+
+# Print out correlation between first and second column. Replace 'None'
+corr = np.corrcoef(np_baseball[:, 0], np_baseball[:, 1])    #compare the first and second columns to see the correlation READ THE INSTRUCTIONS DUMMY
